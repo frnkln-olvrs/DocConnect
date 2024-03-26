590a27b2fef8eb7d14c722c950f282ffe461e3f9
Frankii <franklinoliveros2003@gmail.com>
2024-03-26 22:08:06 +0800

    day 1

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+  <header class="d-flex flex-wrap justify-content-center p-3 mb-4 border-bottom">
+    <a href="./index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+      <i class='bx bx-plus-medical mx-2' height="32"></i>
+      <span class="fs-4 link-primary">DocConnect</span>
+    </a>
+
+    <ul class="nav nav-pills">
+      <li class="nav-item"><a href="#" class="nav-link border-primary">About Us</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">Services</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">Our Doctors</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">Appointment</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+      <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Login</a></li>
+    </ul>
+  </header>
--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home</title>
-</head>
+<?php 
+  $title = 'Home';
+	include './includes/head.php';
+?>
 <body>
-  asdasdasd
+  <?php 
+    require_once ('./includes/header.php');
+  ?>
 </body>
 </html>
