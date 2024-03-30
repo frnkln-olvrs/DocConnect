08d086668db24e40e6b91b4b4348ff2fff50115c
Frankii <franklinoliveros2003@gmail.com>
2024-03-30 21:50:44 +0800

    more information

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <header class="d-flex flex-wrap justify-content-center p-3 border-bottom position-absolute bg-white w-100">
     <a href="./index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-      <i class='bx bx-plus-medical mx-2' height="35"></i>
+      <img src="./img/logo.png" alt="Logo" height="35">
       <h1 class="fs-4 link-primary m-0 d-name">Doc<span class="link-dark">Connect</span></h1>
     </a>
 
@@ -10,6 +10,6 @@
       <li class="nav-item"><a href="#" class="nav-link">Our Doctors</a></li>
       <li class="nav-item"><a href="#" class="nav-link">Appointment</a></li>
       <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
-      <li class="nav-item"><a href="#" class="nav-link active btn-green" aria-current="page">Login</a></li>
+      <li class="nav-item"><a href="#" class="nav-link ms-2 active btn-green" aria-current="page">Login</a></li>
     </ul>
 </header>
--- index.php
+++ index.php
@@ -25,7 +25,7 @@
           </div>
         </div>
         <div class="col-md-6 mt-5">
-          <img src="./img/billboard-img.png" alt="img" class="img-fluid">
+          <img src="./img/billboard-img.png" alt="img" class="img-fluid rounded-circle">
         </div>
       </div>
     </div>
@@ -34,7 +34,7 @@
   <section id="about" class="padding-medium mt-xl-5">
     <div class="container my-4">
       <div class="row align-items-center justify-content-center mt-xl-5">
-        <div class="col-md-5 mt-5 mt-md-0">
+        <div class="col-md-6 mt-5 mt-md-0">
           <div class="mb-3">
             <p class="text-secondary ">Learn more about us</p>
             <h2 class="display-6 fw-semibold">About Us</h2>
@@ -58,12 +58,77 @@
           </div>
           <a href="about.html" class="btn btn-green px-5 py-3 mt-4 link-light">Learn more</a>
         </div>
-        <div class="offset-md-1 col-md-5">
+        <div class="offset-md-1 col-md-4">
           <img src="./img/billboard-img.png" alt="img" class="img-fluid rounded-circle">
         </div>
       </div>
     </div>
   </section>
+
+  <section id="info" class="container py-3">
+    <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+      <h1 class="display-4 fw-normal">Pricing</h1>
+      <p class="fs-5 text-muted">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It’s built with default Bootstrap components and utilities with little customization.</p>
+    </div>
+
+    <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">Free</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$0<small class="text-muted fw-light">/mo</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>10 users included</li>
+              <li>2 GB of storage</li>
+              <li>Email support</li>
+              <li>Help center access</li>
+            </ul>
+            <button type="button" class="w-100 btn btn-lg btn-outline-primary">Sign up for free</button>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">Pro</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$15<small class="text-muted fw-light">/mo</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>20 users included</li>
+              <li>10 GB of storage</li>
+              <li>Priority email support</li>
+              <li>Help center access</li>
+            </ul>
+            <button type="button" class="w-100 btn btn-lg btn-primary">Get started</button>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm border-primary">
+          <div class="card-header py-3 text-white bg-primary border-primary">
+            <h4 class="my-0 fw-normal">Enterprise</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$29<small class="text-muted fw-light">/mo</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>30 users included</li>
+              <li>15 GB of storage</li>
+              <li>Phone and email support</li>
+              <li>Help center access</li>
+            </ul>
+            <button type="button" class="w-100 btn btn-lg btn-primary">Contact us</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<footer class="pt-4 my-md-5 pt-md-5 border-top">
+  <h1 class="d-flex justify-content-center">FOOTER</h1>
+</footer>
 
 </body>
 </html>
