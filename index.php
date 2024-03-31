0824135ad69219e5971709827533ebdea8ceec33
Frankii <franklinoliveros2003@gmail.com>
2024-03-31 13:41:51 +0800

    info again

--- index.php
+++ index.php
@@ -31,8 +31,8 @@
     </div>
   </section>
 
-  <section id="about" class="padding-medium mt-xl-5">
-    <div class="container my-4">
+  <section id="about" class="padding-medium mt-xl-5 py-2">
+    <div class="container mb-4 pt-2">
       <div class="row align-items-center justify-content-center mt-xl-5">
         <div class="col-md-6 mt-5 mt-md-0">
           <div class="mb-3">
@@ -65,7 +65,7 @@
     </div>
   </section>
 
-  <section id="info" class="container py-3">
+  <section id="info" class="padding-medium mt-xl-5 mx-5">
     <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
       <h1 class="display-4 fw-normal">Telemedicine</h1>
       <p class="fs-5 text-muted">Bridging the gap between distance and healthcare accessibility.</p>
@@ -74,62 +74,54 @@
     <div class="row mb-3">
       <div class="col-sm-12 col-md-8">
         <div class="row row-cols-1 row-cols-md-2">
-          <div class="col">
-            <div class="card mb-4 rounded-3 shadow-sm">
-              <div class="card-body">
-                <div class="row">
-                  <div class="col-4">
-                    <i class='bx bx-plus-medical p-3 bg-green text-white rounded-1'></i>
-                  </div>
-                  <div class="col-8">
-                    <h3 class="fw-normal">Doctor on Call</h3>
-                    <p class="fs-6 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem molestiae nam commodi dolore vitae?</p>
-                  </div>
+          <div class="col mb-4">
+            <div class="border border-success shadow-sm rounded-2 p-3">
+              <div class="row">
+                <div class="col-4 d-flex align-items-sm-start align-items-center justify-content-center">
+                  <i class='bx bx-plus-medical p-3 bg-green text-white rounded-1 fs-3'></i>
+                </div>
+                <div class="col-8">
+                  <h3 class="fw-normal">Doctor on Call</h3>
+                  <p class="fs-6 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem molestiae nam commodi dolore vitae?</p>
                 </div>
               </div>
             </div>
           </div>
-          <div class="col">
-            <div class="card mb-4 rounded-3 shadow-sm">
-              <div class="card-body">
-                <div class="row">
-                  <div class="col-4">
-                    <i class='bx bx-plus-medical p-3 border-green text-green rounded-1 shadow-sm'></i>
-                  </div>
-                  <div class="col-8">
-                    <h3 class="fw-normal">Diagnostic Services</h3>
-                    <p class="fs-6 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem molestiae nam commodi dolore vitae?</p>
-                  </div>
+          <div class="col mb-4">
+            <div class="border border-success shadow-sm rounded-2 p-3">
+              <div class="row">
+                <div class="col-4 d-flex align-items-sm-start align-items-center justify-content-center">
+                  <i class='bx bxs-school p-3 border-green text-green rounded-1 shadow-sm fs-3'></i>
+                </div>
+                <div class="col-8">
+                  <h3 class="fw-normal">Doctor on Call</h3>
+                  <p class="fs-6 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem molestiae nam commodi dolore vitae?</p>
                 </div>
               </div>
             </div>
           </div>
-          <div class="col">
-            <div class="card mb-sm-4 mb-sm-0 rounded-3 shadow-sm">
-              <div class="card-body">
-                <div class="row">
-                  <div class="col-4">
-                    <i class='bx bx-plus-medical p-3 border-green text-green rounded-1 shadow-sm'></i>
-                  </div>
-                  <div class="col-8">
-                    <h3 class="fw-normal">Emergency Treatment</h3>
-                    <p class="fs-6 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem molestiae nam commodi dolore vitae?</p>
-                  </div>
+          <div class="col mb-4">
+            <div class="border border-success shadow-sm rounded-2 p-3">
+              <div class="row">
+                <div class="col-4 d-flex align-items-sm-start align-items-center justify-content-center">
+                  <i class='bx bx-plus-medical p-3 border-green text-green rounded-1 shadow-sm fs-3'></i>
+                </div>
+                <div class="col-8">
+                  <h3 class="fw-normal">Doctor on Call</h3>
+                  <p class="fs-6 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem molestiae nam commodi dolore vitae?</p>
                 </div>
               </div>
             </div>
           </div>
-          <div class="col">
-            <div class="card mb-sm-4 mb-sm-0 rounded-3 shadow-sm">
-              <div class="card-body">
-                <div class="row">
-                  <div class="col-4">
-                    <i class='bx bx-plus-medical p-3 bg-green text-white rounded-1'></i>
-                  </div>
-                  <div class="col-8">
-                    <h3 class="fw-normal">Doctor on Call</h3>
-                    <p class="fs-6 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem molestiae nam commodi dolore vitae?</p>
-                  </div>
+          <div class="col mb-4">
+            <div class="border border-success shadow-sm rounded-2 p-3">
+              <div class="row">
+                <div class="col-4 d-flex align-items-sm-start align-items-center justify-content-center">
+                  <i class='bx bxs-capsule p-3 bg-green text-white rounded-1 fs-3'></i>
+                </div>
+                <div class="col-8">
+                  <h3 class="fw-normal">Doctor on Call</h3>
+                  <p class="fs-6 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem molestiae nam commodi dolore vitae?</p>
                 </div>
               </div>
             </div>
@@ -137,9 +129,25 @@
         </div>
       </div>
 
-      <div class="col-sm-12 col-md-4">
-        <div class="bg-primary p-2 rounded-2 h-100 text-white">
-          asdasd
+      <div class="col-sm-12 col-md-4 mb-4">
+        <div class="d-flex flex-column justify-content-between bg-primary p-3 rounded-2 h-100 text-white">
+          <div>
+            <h4>Opening Hours</h4>
+            <p class="fs-6 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem molestiae nam commodi dolore vitae?</p>
+            <div class="d-flex justify-content-between border-bottom mb-2">
+              <p class="mb-2">Mon - Fri</p>
+              <p class="mb-2">7:00am - 6:00pm</p>
+            </div>
+            <div class="d-flex justify-content-between border-bottom mb-2">
+              <p class="mb-2">Sat - Sun</p>
+              <p class="mb-2">7:00am - 4:00pm</p>
+            </div>
+          </div>
+          <div>
+            <div>
+              <button type="button" class="w-100 btn btn-lg btn-outline-light">Book Appointment</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
