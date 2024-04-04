14c230cdde435a0d1cc2c1257434a9cfcdbf2240
Frankii <franklinoliveros2003@gmail.com>
2024-04-04 20:46:54 +0800

    features again

--- user/index.php
+++ user/index.php
@@ -39,7 +39,7 @@
 
     <div class="row row-cols-1 row-cols-md-4">
       <div class="col mb-3">
-        <div class="card mx-3 mb-sm-3 rounded-3 shadow-sm">
+        <div class="card mx-3 mb-sm-3 rounded-3 shadow-sm h-100">
           <div class="card-body d-flex flex-column justify-content-between shadow-sm">
             <div class="align-content-center text-center">
               <i class='bx bx-plus-medical p-3 mb-3 border-green text-green rounded-circle shadow-sm fs-3'></i>
@@ -50,34 +50,34 @@
         </div>
       </div>
       <div class="col mb-3">
-        <div class="card mx-3 mb-sm-3 rounded-3 shadow-sm">
+        <div class="card mx-3 mb-sm-3 rounded-3 shadow-sm h-100">
           <div class="card-body d-flex flex-column justify-content-between shadow-sm">
             <div class="align-content-center text-center">
-              <i class='bx bx-plus-medical p-3 mb-3 border border-2 border-primary text-primary rounded-circle shadow-sm fs-3'></i>
-              <h3 class="card-title pricing-card-title">Enhanced Patient Engagement and Satisfaction</h3>
-              <p class="fs-6 text-muted">Empower patients to participate in their healthcare journey through telecommunication health services.</p>
+              <i class='bx bxs-capsule p-3 mb-3 border border-2 border-primary text-primary rounded-circle shadow-sm fs-3'></i>
+              <h3 class="card-title pricing-card-title">Remote Consultations</h3>
+              <p class="fs-6 text-muted">Access healthcare professionals from anywhere, eliminating the need for in-person visits.</p>
             </div>  
           </div>
         </div>
       </div>
       <div class="col mb-3">
-        <div class="card mx-3 mb-sm-3 rounded-3 shadow-sm">
+        <div class="card mx-3 mb-sm-3 rounded-3 shadow-sm h-100">
           <div class="card-body d-flex flex-column justify-content-between shadow-sm">
             <div class="align-content-center text-center">
               <i class='bx bx-plus-medical p-3 mb-3 border-green text-green rounded-circle shadow-sm fs-3'></i>
-              <h3 class="card-title pricing-card-title">Enhanced Patient Engagement and Satisfaction</h3>
-              <p class="fs-6 text-muted">Empower patients to participate in their healthcare journey through telecommunication health services.</p>
+              <h3 class="card-title pricing-card-title">Specialized Telehealth Services</h3>
+              <p class="fs-6 text-muted">Collaborative care coordination between your primary care provider and specialists for comprehensive treatment plans.</p>
             </div>  
           </div>
         </div>
       </div>
       <div class="col mb-3">
-        <div class="card mx-3 mb-sm-3 rounded-3 shadow-sm">
+        <div class="card mx-3 mb-sm-3 rounded-3 shadow-sm h-100">
           <div class="card-body d-flex flex-column justify-content-between shadow-sm">
             <div class="align-content-center text-center">
               <i class='bx bx-plus-medical p-3 mb-3 border border-2 border-primary text-primary rounded-circle shadow-sm fs-3'></i>
-              <h3 class="card-title pricing-card-title">Enhanced Patient Engagement and Satisfaction</h3>
-              <p class="fs-6 text-muted">Empower patients to participate in their healthcare journey through telecommunication health services.</p>
+              <h3 class="card-title pricing-card-title">Scalable Telehealth Solutions for Healthcare Providers</h3>
+              <p class="fs-6 text-muted">Customizable telehealth platforms tailored to the needs of individual healthcare practices.</p>
             </div>  
           </div>
         </div>
