ac6e3df8a64a9cc39651af0a7924cec22d938746
Frankii <franklinoliveros2003@gmail.com>
2024-04-04 12:30:31 +0800

    idex

--- includes/header.php
+++ includes/header.php
@@ -1,15 +1,49 @@
 <header class="d-flex flex-wrap justify-content-center p-3 border-bottom position-absolute bg-white w-100">
+  <a href="./index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+    <img src="../assets/images/logo.png" alt="Logo" height="35">
+    <h1 class="fs-4 link-primary m-0 d-name">Doc<span class="link-dark">Connect</span></h1>
+  </a>
+
+  <ul class="header nav nav-pills">
+    <li class="nav-item"><a href="#about" class="nav-link">About Us</a></li>
+    <li class="nav-item"><a href="#" class="nav-link">Services</a></li>
+    <li class="nav-item"><a href="#" class="nav-link">Our Doctors</a></li>
+    <li class="nav-item"><a href="#" class="nav-link">Appointment</a></li>
+    <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+    <li class="nav-item"><a href="#" class="nav-link ms-2 bg-green text-white" aria-current="page">Login</a></li>
+  </ul>
+</header>
+
+<!-- <header class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
     <a href="./index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
       <img src="../assets/images/logo.png" alt="Logo" height="35">
       <h1 class="fs-4 link-primary m-0 d-name">Doc<span class="link-dark">Connect</span></h1>
     </a>
-
-    <ul class="header nav nav-pills">
-      <li class="nav-item"><a href="#" class="nav-link">About Us</a></li>
-      <li class="nav-item"><a href="#" class="nav-link">Services</a></li>
-      <li class="nav-item"><a href="#" class="nav-link">Our Doctors</a></li>
-      <li class="nav-item"><a href="#" class="nav-link">Appointment</a></li>
-      <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
-      <li class="nav-item"><a href="#" class="nav-link ms-2 bg-green text-white" aria-current="page">Login</a></li>
-    </ul>
-</header>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="collapse navbar-collapse" id="navbarNavDropdown">
+        <li class="nav-item">
+          <a href="#about" class="nav-link">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Services</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Our Doctors</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Appointment</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link ms-2 bg-green text-white" aria-current="page">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</header> -->
--- user/index.php
+++ user/index.php
@@ -24,7 +24,7 @@
 
           </div>
         </div>
-        <div class="col-md-6 mt-5">
+        <div class="img col-md-6 mt-5">
           <img src="../assets/images/billboard-img.png" alt="img" class="img-fluid rounded-circle">
         </div>
       </div>
@@ -34,50 +34,49 @@
   <section id="services" class="padding-medium mt-xl-5 mx-5">
     <div class="p-3 pb-md-4 mx-auto text-center">
       <p class="fs-5 text-muted text-uppercase mx-5">What we do</p>
-      <h1 class="display-6 fw-normal">Providing medical care for the </br> sickest in our University</h1>
+      <h1 class="display-6 fw-normal">Providing medical care for the <br> sickest in our University</h1>
     </div>
 
-    <div>
-      <div class="row row-cols-1 row-cols-md-3">
-        <div class="col">
-          <img src="../assets/images/sevices_img1.jpeg" alt="sevices_img1.jpeg" class="rounded-2">
-          <div class="card mx-3 rounded-3 shadow-sm">
-            <div class="card-body d-flex flex-column justify-content-between shadow-sm">
-              <div>
-                <h3 class="card-title pricing-card-title">Second Opinions</h3>
-                <p class="fs-6 text-muted">Seek expert medical opinions from specialists across various fields without the need for in-person visits.</p>
-              </div>
-              <button type="button" class="w-100 btn btn-outline-primary">Read More</button>
+    <div class="row row-cols-1 row-cols-md-3">
+      <div class="col mb-4">
+        <img src="../assets/images/sevices_img1.jpeg" alt="sevices_img1.jpeg" class="rounded-2 shadow-lg">
+        <div class="card mx-3 mb-sm-3 rounded-3 shadow-sm">
+          <div class="card-body d-flex flex-column justify-content-between shadow-sm">
+            <div>
+              <h3 class="card-title pricing-card-title">Second Opinions</h3>
+              <p class="fs-6 text-muted">Seek expert medical opinions from specialists across various fields without the need for in-person visits.</p>
             </div>
+            <button type="button" class="w-100 btn btn-outline-primary">Read More</button>
           </div>
         </div>
-        <div class="col">
-          <img src="../assets/images/sevices_img2.jpeg" alt="sevices_img1.jpeg" class="rounded-2 border-green border-5">
-          <div class="card mx-3 rounded-3 shadow-sm">
-            <div class="card-body d-flex flex-column justify-content-between shadow-sm">
-              <div>
-                <h3 class="card-title pricing-card-title">Accessibility</h3>
-                <p class="fs-6 text-muted">Reach healthcare providers easily, even in remote or underserved areas, ensuring equitable access to medical care.</p>
-              </div>
-              <button type="button" class="w-100 btn btn-outline-primary">Read More</button>
+      </div>
+      <div class="col mb-4">
+        <img src="../assets/images/sevices_img2.jpeg" alt="sevices_img1.jpeg" class="rounded-2 shadow-lg">
+        <div class="card mx-3 mb-sm-3 rounded-3 shadow-sm">
+          <div class="card-body d-flex flex-column justify-content-between shadow-sm">
+            <div>
+              <h3 class="card-title pricing-card-title">Accessibility</h3>
+              <p class="fs-6 text-muted">Reach healthcare providers easily, even in remote or underserved areas, ensuring equitable access to medical care.</p>
             </div>
+              <button type="button" class="w-100 btn btn-outline-primary">Read More</button>
           </div>
         </div>
-        <div class="col">
-          <img src="../assets/images/sevices_img3.jpeg" alt="sevices_img1.jpeg" class="rounded-2">
-          <div class="card mx-3 rounded-3 shadow-sm">
-            <div class="card-body d-flex flex-column justify-content-between shadow-sm">
-              <div>
-                <h3 class="card-title pricing-card-title">Patient Portal</h3>
-                <p class="fs-6 text-muted">Manage appointments, view medical records, and communicate securely with healthcare providers through a user-friendly online portal.</p>
-              </div>
-              <button type="button" class="w-100 btn btn-outline-primary">Read More</button>
+      </div>
+      <div class="col mb-4">
+        <img src="../assets/images/sevices_img3.jpeg" alt="sevices_img1.jpeg" class="rounded-2 shadow-lg">
+        <div class="card mx-3 mb-sm-3 rounded-3 shadow-sm">
+          <div class="card-body d-flex flex-column justify-content-between shadow-sm">
+            <div>
+              <h3 class="card-title pricing-card-title">Patient Portal</h3>
+              <p class="fs-6 text-muted">Manage appointments, view medical records, and communicate securely with healthcare providers through a user-friendly online portal.</p>
             </div>
+            <button type="button" class="w-100 btn btn-outline-primary">Read More</button>
           </div>
         </div>
       </div>
     </div>
   </section>
+
 
   <section id="info" class="padding-medium mt-xl-5 mx-5">
     <div class="p-3 pb-md-4 mx-auto text-center">
@@ -159,7 +158,7 @@
           </div>
           <div>
             <div>
-              <button type="button" class="w-100 btn btn-lg btn-outline-light">Book Appointment</button>
+              <button type="button" class="w-100 btn btn-lg btn-outline-light mt-2">Book Appointment</button>
             </div>
           </div>
         </div>
