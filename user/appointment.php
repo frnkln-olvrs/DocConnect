dcb9b8760498b63e9011a403530733a16a5e3232
Frankii <franklinoliveros2003@gmail.com>
2024-04-10 12:57:08 +0800

    appointment
    remove row - col
    use d-flex

--- user/appointment.php
+++ user/appointment.php
@@ -13,65 +13,59 @@
     <div class="row mb-3">
       <div class="col-sm-12 col-md-8">
         <div class="border border-dark-subtle shadow-sm rounded-2 p-3">
-          <p class="fs-5">Select Date - <span class="text-muted fs-6">April 2024</span></p>
-          <div class="row row-cols-3 row-cols-md-6">
-
-            <div class="col mb-3">
+          <div>
+            <p class="fs-5">Select Date - <span class="text-muted fs-6">April 2024</span></p>
+            <div class="d-flex flex-row flex-wrap justify-content-evenly justify-content-md-start">
+  
               <label>
                 <input class="radio-input shadow-none" type="radio" name="date">
-                <div class="radio-tile p-2 d-flex justify-content-around me-2">
+                <div class="radio-tile p-2 d-flex justify-content-around mb-2 me-md-3 me-2">
                   <span class="radio-label">WED</span>
                   <span class="radio-label">10</span>
                 </div>
               </label>
-            </div>
-            <div class="col mb-3">
               <label>
-                <input class="radio-input" type="radio" name="date">
-                <div class="radio-tile p-2 d-flex justify-content-around me-2">
-                  <span class="radio-label">THU</span>
-                  <span class="radio-label">11</span>
+                <input class="radio-input shadow-none" type="radio" name="date">
+                <div class="radio-tile p-2 d-flex justify-content-around mb-2 me-md-3 me-2">
+                  <span class="radio-label">WED</span>
+                  <span class="radio-label">10</span>
                 </div>
               </label>
-            </div>
-            <div class="col mb-3">
               <label>
-                <input class="radio-input" type="radio" name="date">
-                <div class="radio-tile p-2 d-flex justify-content-around me-2">
-                  <span class="radio-label">FRI</span>
-                  <span class="radio-label">12</span>
+                <input class="radio-input shadow-none" type="radio" name="date">
+                <div class="radio-tile p-2 d-flex justify-content-around mb-2 me-md-3 me-2">
+                  <span class="radio-label">WED</span>
+                  <span class="radio-label">10</span>
                 </div>
               </label>
-            </div>
-            <div class="col mb-3">
               <label>
-                <input class="radio-input" type="radio" name="date">
-                <div class="radio-tile p-2 d-flex justify-content-around me-2">
-                  <span class="radio-label">SAT</span>
-                  <span class="radio-label">13</span>
+                <input class="radio-input shadow-none" type="radio" name="date">
+                <div class="radio-tile p-2 d-flex justify-content-around mb-2 me-md-3 me-2">
+                  <span class="radio-label">WED</span>
+                  <span class="radio-label">10</span>
                 </div>
               </label>
-            </div>
-            <div class="col mb-3">
               <label>
-                <input class="radio-input" type="radio" name="date">
-                <div class="radio-tile p-2 d-flex justify-content-around me-2">
-                  <span class="radio-label">SUN</span>
-                  <span class="radio-label">14</span>
+                <input class="radio-input shadow-none" type="radio" name="date">
+                <div class="radio-tile p-2 d-flex justify-content-around mb-2 me-md-3 me-2">
+                  <span class="radio-label">WED</span>
+                  <span class="radio-label">10</span>
                 </div>
               </label>
-            </div>
-            <div class="col mb-3">
               <label>
-                <input class="radio-input" type="radio" name="date">
-                <div class="radio-tile p-2 d-flex justify-content-around me-2">
-                  <span class="radio-label">MON</span>
-                  <span class="radio-label">15</span>
+                <input class="radio-input shadow-none" type="radio" name="date">
+                <div class="radio-tile p-2 d-flex justify-content-around mb-2 me-md-3 me-2">
+                  <span class="radio-label">WED</span>
+                  <span class="radio-label">10</span>
                 </div>
               </label>
+  
             </div>
-
           </div>
+
+          <hr>
+
+
         </div>
       </div>
 
