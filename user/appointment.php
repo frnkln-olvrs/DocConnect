79f7190546a25bf3576ec6b0e484d69d88746255
Frankii <franklinoliveros2003@gmail.com>
2024-04-14 14:42:21 +0800

    Update appointment.php

--- user/appointment.php
+++ user/appointment.php
@@ -12,7 +12,7 @@
   <section id="info" class="padding-medium mt-5 mx-5">
     <div class="row mb-3">
       <div class="col-sm-12 col-md-8">
-        <div class="border border-dark-subtle shadow-sm rounded-2 p-3">
+        <div class="border border-dark-subtle shadow-sm rounded-2 p-3 mb-4 mb-md-0">
           <div>
             <p class="fs-5">Select Date - <span class="text-muted fs-6">April 2024</span></p>
             <div class="d-flex flex-row flex-wrap justify-content-evenly justify-content-md-start">
@@ -59,7 +59,11 @@
                   <span class="radio-label">15</span>
                 </div>
               </label>
-  
+              
+            </div>
+            <div class="d-flex flex-column mt-2">
+              <label for="extra_date mb-2">Other date:</label>
+              <input type="date" name="extra_date" class="mt-2 px-2 py-1 bg-white border border-primary rounded-1 w-25 ">
             </div>
           </div>
 
@@ -133,33 +137,32 @@
                 <p class="fs-6 fw-light text-muted">Enter the number on which you wish to recieve checkup related information</p>
                 <div class="d-flex flex-row flex-wrap justify-content-evenly justify-content-md-start me-md-5 pe-md-5">
       
-                  <input type="number" class="form-control border border-dark" placeholder="09" aria-label="mobile_no" aria-describedby="mobile_no">
+                  <input type="number" class="form-control border border-dark" placeholder="09" aria-label="mobile_no" aria-describedby="mobile_no" value="09">
       
                 </div>
               </div>
             </div>
           </div>
-
         </div>
       </div>
 
-      <div class="col-sm-12 col-md-4 mb-4">
-        <div class="d-flex flex-column justify-content-between bg-primary p-3 rounded-2 h-100 text-white">
+      <div class="col-sm-12 col-md-4 h-100">
+        <div class="d-flex flex-column justify-content-between bg-green p-3 rounded-2 h-100 text-white">
           <div>
-            <h4>Opening Hours</h4>
-            <p class="fs-6 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem molestiae nam commodi dolore vitae?</p>
+            <h4>Appointment Details</h4>
+            <p class="fs-6 text-white">Details:</p>
             <div class="d-flex justify-content-between border-bottom mb-2">
-              <p class="mb-2">Mon - Fri</p>
-              <p class="mb-2">7:00am - 6:00pm</p>
+              <p class="mb-2">Saturday - 13th of April</p>
+              <p class="mb-2">09:00am - 10:00am</p>
             </div>
-            <div class="d-flex justify-content-between border-bottom mb-2">
-              <p class="mb-2">Sat - Sun</p>
-              <p class="mb-2">7:00am - 4:00pm</p>
+            <div class="d-flex flex-column justify-content-between border-bottom mb-2">
+              <p class="mb-2">Gmeet link:</p>
+              <a href="https://meet.google.com/por-udiy-etd" class="mb-2 link-light">https://meet.google.com/por-udiy-etd</a>
             </div>
           </div>
           <div>
             <div>
-              <button type="button" class="w-100 btn btn-lg btn-outline-light mt-2">Book Appointment</button>
+              <button type="button" class="w-100 btn btn-lg btn-outline-light mt-2">Enter Meeting</button>
             </div>
           </div>
         </div>
