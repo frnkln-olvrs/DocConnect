b9e2f0c562bc67c0a305454c2db5f9aeef46303f
Frankii <franklinoliveros2003@gmail.com>
2024-04-16 09:31:55 +0800

    chatbot?
    unfinsihed??

--- user/chat.php
+++ user/chat.php
@@ -34,8 +34,8 @@
           </div>
         </div>
 
-        <div class="body d-flex align-items-end bg-light p-2">
-          <div class="d-flex align-items-end">
+        <div class="body d-flex flex-column justify-content-end bg-light p-2">
+          <div class="d-flex align-items-end mb-2">
             <div class="bg-secondary-subtle p-2 me-3 rounded-5">
               <i class='bx bxs-bot fs-3 text-primary'></i>
             </div>
@@ -43,11 +43,19 @@
               <p class="fw-light mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem molestiae nam commodi dolore vitae?</p>
             </div>
           </div>
+          <div class="d-flex justify-content-end align-items-end">
+            <div id="chat_text" class="bg-secondary-subtle p-2 rounded-3">
+              <p class="fw-light mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem molestiae nam commodi dolore vitae?</p>
+            </div>
+            <div class="bg-secondary-subtle p-2 ms-3 rounded-5">
+              <i class='bx bxs-bot fs-3 text-primary'></i>
+            </div>
+          </div>
         </div>
 
         <div class="chat_input pt-4 d-flex justify-content-end align-items-center p-2 bg-light">
-          <input type="text" name="" id="" class="p-2 ps-4 rounded-5 w-100 fs-6 bg-secondary-subtle">
-          <i class='bx bx-send text-dark ps-3 pe-1 fs-4' ></i>
+          <input type="text" name="" id="" placeholder="Aa" class="p-2 ps-4 rounded-5 w-100 fs-6 bg-secondary-subtle">
+          <button class="bg-light" ><i class='bx bx-send text-dark ps-3 pe-1 fs-4' ></i></button>
         </div>
       </div>
     </div>
