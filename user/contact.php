df9a4e2a93ca1160cd495d249ebbcc04f9286acf
Frankii <franklinoliveros2003@gmail.com>
2024-06-05 15:03:19 +0800

    Update contact.php

--- user/contact.php
+++ user/contact.php
@@ -9,7 +9,20 @@
     require_once ('../includes/header.php');
   ?>
 
-  
+  <section class="main">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-6 pe-3 pe-md-5 mt-5 mt-md-0">
+          <h2 class="display-1 text-uppercase text-light text-center text-md-start">Get in Touch</h2>
+          <p class="fs-4 my-4 pb-2 text-white text-center text-md-start">Want to get in touch? we’d love to hear from you. here’s how you can reach us</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    
+  </section>
 
   <?php 
     require_once ('../includes/footer.php');
