79f573a5cb2528759c4306864c9cac76838d92b6
Frankii <franklinoliveros2003@gmail.com>
2024-07-22 21:48:42 +0800

    Update admin_header.php

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,20 +1,10 @@
-<header class="p-3 mb-3 border-bottom">
-  <div class="container">
-    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-      <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
-      </a>
-
-      <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="#" class="nav-link px-2 link-secondary">Overview</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">Inventory</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">Customers</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">Products</a></li>
-      </ul>
-
-      <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-        <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
-      </form>
+<header class="p-3 border-bottom">
+  <div class="mx-1 mx-lg-3">
+    <div class="d-flex flex-wrap justify-content-between">
+    <a class="navbar-brand d-flex align-items-center text-dark text-decoration-none" href="../index.php">
+      <img src="../assets/images/logo.png" alt="Logo" height="35">
+      <h1 class="fs-4 link-primary m-0 d-name">Doc<span class="link-dark">Connect</span></h1>
+		</a>
 
       <div class="dropdown text-end">
         <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
