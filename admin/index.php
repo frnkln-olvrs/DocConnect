8a77c9ed9c8831130b730fc7399fff4c8aa8c126
Frankii <franklinoliveros2003@gmail.com>
2024-07-22 17:16:12 +0800

    admin header

---
+++ admin/index.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<?php 
+  $title = 'Admin | Dashboard';
+	include '../includes/head.php';
+?>
+<body>
+  <?php 
+    require_once ('./includes/admin_header.php');
+  ?>
+</body>
+</html>
