15a5028cd6117d9c41a7c888c5d8dd86babe5409
Frankii <franklinoliveros2003@gmail.com>
2024-07-23 17:43:02 +0800

    Update admin_header.php

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,14 +1,14 @@
-<header class="p-3 border-bottom">
+<header class="p-3 border-bottom bg-success shadow-lg">
   <div class="mx-1 mx-lg-3">
     <div class="d-flex flex-wrap justify-content-between">
     <a class="navbar-brand d-flex align-items-center text-dark text-decoration-none" href="../index.php">
-      <img src="../assets/images/logo.png" alt="Logo" height="35">
-      <h1 class="fs-4 link-primary m-0 d-name">Doc<span class="link-dark">Connect</span></h1>
+      <img src="../assets/images/logo.png" class="me-2" alt="Logo" height="32">
+      <h1 class="fs-4 link-light m-0 d-name">Doc<span class="link-light">Connect</span></h1>
 		</a>
 
       <div class="dropdown text-end">
         <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-          <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+          <img src="./../assets/images/profile_img.jpg" alt="mdo" width="32" height="32" class="rounded-circle">
         </a>
         <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
           <li><a class="dropdown-item" href="#">New project...</a></li>
