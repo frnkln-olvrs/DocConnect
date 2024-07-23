9e88373cd2aa55a46590447c57f6c495e0e66359
Frankii <franklinoliveros2003@gmail.com>
2024-07-23 17:36:11 +0800

    Update admin_sidepanel.php

--- admin/includes/admin_sidepanel.php
+++ admin/includes/admin_sidepanel.php
@@ -1,33 +1,45 @@
-<div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px; height: calc(100% - 67.7px)">
+<div class="d-flex flex-column flex-shrink-0 p-3 bg-light border-end border-2" style="width: 250px; height: calc(100% - 67.7px)">
   <ul class="nav nav-pills flex-column mb-auto">
-    <li class="nav-item">
-      <a href="#" class="nav-link active" aria-current="page">
-        <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
-        Home
-      </a>
-    </li>
     <li>
-      <a href="#" class="nav-link link-dark">
-        <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
+      <a href="#" class="nav-link active" aria-current="page">
+        <i class='bx bx-home-alt me-2'></i>
         Dashboard
       </a>
     </li>
     <li>
       <a href="#" class="nav-link link-dark">
-        <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
-        Orders
+        <i class='bx bx-user me-2' ></i>
+        Users
       </a>
     </li>
     <li>
       <a href="#" class="nav-link link-dark">
-        <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
-        Products
+        <i class='bx bx-buildings me-2' ></i>
+        Campuses
       </a>
     </li>
     <li>
       <a href="#" class="nav-link link-dark">
-        <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
-        Customers
+        <i class='bx bx-calendar me-2' ></i>
+        Appointment
+      </a>
+    </li>
+    <li>
+      <a href="#" class="nav-link link-dark">
+        <i class='bx bx-chart me-2' ></i>
+        Analytics & Reports
+      </a>
+    </li>
+    <li>
+      <a href="#" class="nav-link link-dark">
+        <i class='bx bx-group me-2' ></i>
+        Staff
+      </a>
+    </li>
+    <li>
+      <a href="#" class="nav-link link-dark">
+        <i class='bx bx-cog me-2' ></i>
+        Settings
       </a>
     </li>
   </ul>
