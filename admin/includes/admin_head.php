7cf2c355634232fe7b0e452c4402a1a13d5d878d
Frankii <franklinoliveros2003@gmail.com>
2024-07-25 16:06:40 +0800

    Update admin_head.php

--- admin/includes/admin_head.php
+++ admin/includes/admin_head.php
@@ -5,9 +5,9 @@
   <link rel="icon" href="../../assets/images/logo.png">
 
   <!-- bootstrap-5 -->
-  <link rel="stylesheet" href="../../nonode_modules/bootstrap/dist/css/bootstrap.min.css">
-  <script src="../../vendor/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
+  <script src="../vendor/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   
@@ -18,5 +18,6 @@
   <!-- JQuery -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.js"></script>
 
+  <link rel="stylesheet" href="../css/custom.min.css">
   <link rel="stylesheet" href="./css/style.css">
 </head>
