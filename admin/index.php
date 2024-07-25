e060a51c1e7593516740a7ec963747f9b3529aeb
Frankii <franklinoliveros2003@gmail.com>
2024-07-25 16:31:06 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -16,24 +16,40 @@
 
     <div class="container">
       <div class="row">
-        <div class="col bg-primary mx-2">
-          <div>
-            <i class='bx bx-user'></i>
-            <p class="">1,100</p>
-            <p class="">Total Users</p>
+        <div class="col bg-primary mx-2 p-3 text-white">
+          <div class="row g-3">
+            <div class="col-3">
+              <i class='bx bx-user'></i>
+            </div>
+            <div class="col-9">
+              <p class="">1,100</p>
+              <p class="">Total Users</p>
+            </div>
           </div>
         </div>
 
-        <div class="col bg-primary mx-2">
-          <i class='bx bx-user-check'></i>
-          <p class="">35</p>
-          Total Active Users
+        <div class="col bg-primary mx-2 p-3 text-white">
+          <div class="row g-3">
+            <div class="col-3">
+              <i class='bx bx-user-check'></i>
+            </div>
+            <div class="col-9">
+              <p class="">35</p>
+              <p class="">Total Active Users</p>
+            </div>
+          </div>
         </div>
 
-        <div class="col bg-primary mx-2">
-          <i class='bx bx-user-plus'></i>
-          <p class="">659</p>
-          Total Patients
+        <div class="col bg-primary mx-2 p-3 text-white">
+          <div class="row g-3">
+            <div class="col-3">
+              <i class='bx bx-user-plus'></i>
+            </div>
+            <div class="col-9">
+              <p class="">659</p>
+              <p class="">Total Patients</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
