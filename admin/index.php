38e1116895ddaab78e488203adce23e8e9063ce5
Frankii <franklinoliveros2003@gmail.com>
2024-08-02 19:16:17 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -58,23 +58,23 @@
       <div class="row mx-2">
         <div class="col-4">
           <div class="border border-2 border-dark-subtle shadow-sm p-3 h-100">
-            akjshdkajsd
+            <h3 class="text-center mb-4">Appoint Manangement</h3>
+            <h5>Apointments today: </h5>
           </div>
         </div>
         
         <div class="col-md-8">
-          
           <nav>
 		      	<div class="nav nav-tabs mb-3" id="nav-tab" role="tablist">
-		      		<button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Campus</button>
-		      		<button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Type</button>
+		      		<button class="nav-link  active" id="nav-campus-tab" data-bs-toggle="tab" data-bs-target="#nav-campus" type="button" role="tab" aria-controls="nav-campus" aria-selected="true">Campus</button>
+		      		<button class="nav-link" id="nav-type-tab" data-bs-toggle="tab" data-bs-target="#nav-type" type="button" role="tab" aria-controls="nav-type" aria-selected="false">Type</button>
 		      	</div>
 		      </nav>
-		      <div class="tab-content p-3 border bg-light" id="nav-tabContent">
-		      	<div class="tab-pane fade active show" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+		      <div class="tab-content p-3 border bg-white" id="nav-tabContent">
+		      	<div class="tab-pane fade active show" id="nav-campus" role="tabpanel" aria-labelledby="nav-campus-tab">
               <canvas id="campusChart" style="width:100%;max-width:700px"></canvas>
 		      	</div>
-		      	<div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+		      	<div class="tab-pane fade" id="nav-type" role="tabpanel" aria-labelledby="nav-type-tab">
               <canvas id="typeChart" style="width:100%;max-width:700px"></canvas>
 		      	</div>
 		      </div>
