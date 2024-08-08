80556b2c2a1f4bc9c7ae584a586961e0c0b029f4
Frankii <franklinoliveros2003@gmail.com>
2024-08-08 15:53:04 +0800

    userssssss pageeeeee updateddddd

--- admin/admin-user.php
+++ admin/admin-user.php
@@ -19,13 +19,13 @@
 
     <div class="container text-center mt-5 px-5">
       <ul class="list-group">
-        <a href="#">
-          <li class="list-group-item my-2 p-2 border-0">
+        <a href="#" class="d-flex justify-content-center">
+          <li class="list-group-item my-2 p-2 border-0 w-75">
             <div class="row align-items-center justify-content-center">
               <div class="col-1">
                 <i class='bx bx-user-pin text-danger' ></i>
               </div>
-              <div class="col-10 text-start ms-3 w-25">
+              <div class="col-10 text-start ms-3 w-50">
                 <p class="fs-3 mb-0">Doctors Accounts</p>
               </div>
               <div class="col-1">
@@ -35,13 +35,13 @@
           </li>
         </a>
 
-        <a href="#">
-          <li class="list-group-item my-2 p-2 border-0">
+        <a href="#" class="d-flex justify-content-center">
+          <li class="list-group-item my-2 p-2 border-0 w-75">
             <div class="row align-items-center justify-content-center">
               <div class="col-1">
                 <i class='bx bx-user text-danger'></i>
               </div>
-              <div class="col-10 text-start ms-3 w-25">
+              <div class="col-10 text-start ms-3 w-50">
                 <p class="fs-3 mb-0">Patients Accounts</p>
               </div>
               <div class="col-1">
