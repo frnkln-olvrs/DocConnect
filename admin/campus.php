ef210cf299ef9007bae529756b63e9b744edfe53
Frankii <franklinoliveros2003@gmail.com>
2024-08-10 16:06:01 +0800

    Update campus.php

--- admin/campus.php
+++ admin/campus.php
@@ -17,50 +17,59 @@
   <section id="campus">
     <h1 class="text-center my-3">Campuses</h1>
 
-    <div class="row row-cols-1 row-cols-md-4">
+    <div class="row row-cols-1 row-cols-md-2 mx-1 mx-md-5">
       <div class="col p-0 mb-3">
-        <div class="card mx-3 mb-sm-3 rounded-3 shadow-sm h-100">
+        <a href="#" class="card mx-3 mb-sm-3 rounded-3 shadow-sm h-100">
           <div class="card-body d-flex flex-column justify-content-between shadow-sm">
             <div class="align-content-center text-center">
-              <i class='bx bx-plus-medical p-3 mb-3 border-green text-green rounded-circle shadow-sm fs-3'></i>
-              <h4 class="card-title pricing-card-title">Enhanced Patient Engagement and Satisfaction</h4>
-              <p class="fs-6 text-muted">Empower patients to participate in their healthcare journey through telecommunication health services.</p>
+              <img src="../assets/images/bg-1.png" alt="" class="w-100 rounded-2">
+              <div class="d-flex mt-2 align-items-center">
+                <i class='bx bxs-school fs-2'></i>
+                <p class="m-0 ms-3 fs-4">Campus A</p>
+              </div>
+              <div class="d-flex mt-2 align-items-center">
+                <i class='bx bxs-map fs-2'></i>
+                <p class="m-0 ms-3 fs-4 text-start">Location</p>
+              </div>
             </div>  
           </div>
-        </div>
+        </a>
       </div>
+
       <div class="col p-0 mb-3">
-        <div class="card mx-3 mb-sm-3 rounded-3 shadow-sm h-100">
+        <a href="#" class="card mx-3 mb-sm-3 rounded-3 shadow-sm h-100">
           <div class="card-body d-flex flex-column justify-content-between shadow-sm">
             <div class="align-content-center text-center">
-              <i class='bx bxs-capsule p-3 mb-3 border border-2 border-primary text-primary rounded-circle shadow-sm fs-3'></i>
-              <h4 class="card-title pricing-card-title">Remote Consultations</h4>
-              <p class="fs-6 text-muted">Access healthcare professionals from anywhere, eliminating the need for in-person visits.</p>
+              <img src="../assets/images/bg-1.png" alt="" class="w-100 rounded-2">
+              <div class="d-flex mt-2 align-items-center">
+                <i class='bx bxs-school fs-2'></i>
+                <p class="m-0 ms-3 fs-4">Campus B</p>
+              </div>
+              <div class="d-flex mt-2 align-items-center">
+                <i class='bx bxs-map fs-2'></i>
+                <p class="m-0 ms-3 fs-4 text-start">Location</p>
+              </div>
             </div>  
           </div>
-        </div>
+        </a>
       </div>
+
       <div class="col p-0 mb-3">
-        <div class="card mx-3 mb-sm-3 rounded-3 shadow-sm h-100">
+        <a href="#" class="card mx-3 mb-sm-3 rounded-3 shadow-sm h-100">
           <div class="card-body d-flex flex-column justify-content-between shadow-sm">
             <div class="align-content-center text-center">
-              <i class='bx bx-plus-medical p-3 mb-3 border-green text-green rounded-circle shadow-sm fs-3'></i>
-              <h4 class="card-title pricing-card-title">Specialized Telehealth Services</h4>
-              <p class="fs-6 text-muted">Collaborative care coordination between your primary care provider and specialists for comprehensive treatment plans.</p>
+              <img src="../assets/images/bg-1.png" alt="" class="w-100 rounded-2">
+              <div class="d-flex mt-2 align-items-center">
+                <i class='bx bxs-school fs-2'></i>
+                <p class="m-0 ms-3 fs-4">Campus C</p>
+              </div>
+              <div class="d-flex mt-2 align-items-center">
+                <i class='bx bxs-map fs-2'></i>
+                <p class="m-0 ms-3 fs-4 text-start">Location</p>
+              </div>
             </div>  
           </div>
-        </div>
-      </div>
-      <div class="col p-0 mb-3">
-        <div class="card mx-3 mb-sm-3 rounded-3 shadow-sm h-100">
-          <div class="card-body d-flex flex-column justify-content-between shadow-sm">
-            <div class="align-content-center text-center">
-              <i class='bx bx-plus-medical p-3 mb-3 border border-2 border-primary text-primary rounded-circle shadow-sm fs-3'></i>
-              <h4 class="card-title pricing-card-title">Scalable Telehealth Solutions for Healthcare Providers</h4>
-              <p class="fs-6 text-muted">Customizable telehealth platforms tailored to the needs of individual healthcare practices.</p>
-            </div>  
-          </div>
-        </div>
+        </a>
       </div>
     </div>
   </section>
