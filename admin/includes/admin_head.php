2bc5ec92581f507d85127294fde61e2b047936b8
Frankii <franklinoliveros2003@gmail.com>
2024-08-10 18:26:31 +0800

    view campuses

--- admin/includes/admin_head.php
+++ admin/includes/admin_head.php
@@ -10,7 +10,7 @@
   <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-  
+
   <!-- boxicon -->
   <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
@@ -18,7 +18,11 @@
   <!-- JQuery -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.js"></script>
 
+  <!-- Charts -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+
+  <!-- uiBall -->
+  <script type="module" src="https://cdn.jsdelivr.net/npm/ldrs/dist/auto/spiral.js"></script>
 
   <link rel="stylesheet" href="../css/custom.min.css">
   <link rel="stylesheet" href="./css/style.css">
---
+++ admin/view_campus.php
@@ -0,0 +1,28 @@
+<html lang="en">
+<?php 
+  $title = 'Campuses | Campus A';
+	include './includes/admin_head.php';
+  function getCurrentPage() {
+    return basename($_SERVER['PHP_SELF']);
+  }
+?>
+<body>
+  <?php 
+    require_once ('./includes/admin_header.php');
+  ?>
+  <?php 
+    require_once ('./includes/admin_sidepanel.php');
+  ?>
+
+  <section id="campus">
+    <h1 class="text-center my-3">Campuses</h1>
+
+    <div class="d-flex justify-content-center w-100 h-50">
+      <img src="../assets/images/bg-1.png" alt="" class="w-75 rounded-2">
+    </div>
+
+    
+  </section>
+
+</body>
+</html>
