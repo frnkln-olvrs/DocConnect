349721bbdc98c8d819bddce4db1c6e8535a3f975
Frankii <franklinoliveros2003@gmail.com>
2024-08-10 19:01:36 +0800

    Update view_campus.php

--- admin/view_campus.php
+++ admin/view_campus.php
@@ -21,7 +21,17 @@
       <img src="../assets/images/bg-1.png" alt="" class="w-75 rounded-2">
     </div>
 
-    
+    <div class="campus_name text-center my-2">
+      <h2>Campus A</h2>
+    </div>
+
+    <i class='bx bx-map'></i> <!-- Address Icon -->
+<i class='bx bx-phone'></i> <!-- Contact Number Icon -->
+<i class='bx bx-envelope'></i> <!-- Email Icon -->
+<i class='bx bx-map-alt'></i> <!-- Campus Map Icon -->
+<i class='bx bx-building'></i> <!-- Facilities Icon -->
+<i class='bx bx-book-content'></i> <!-- Programs Offered Icon -->
+
   </section>
 
 </body>
