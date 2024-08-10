70c37149f03abb13a83ab394655c8f03253d18e2
Frankii <franklinoliveros2003@gmail.com>
2024-08-10 18:24:12 +0800

    Update campus.php

--- admin/campus.php
+++ admin/campus.php
@@ -19,17 +19,19 @@
 
     <div class="row row-cols-1 row-cols-md-2 mx-1 mx-md-5">
       <div class="col p-0 mb-3">
-        <a href="#" class="card mx-3 mb-sm-3 rounded-3 shadow-sm h-100">
+        <a href="./view_campus" class="card mx-3 mb-sm-3 rounded-3 shadow-sm h-100">
           <div class="card-body d-flex flex-column justify-content-between shadow-sm">
             <div class="align-content-center text-center">
               <img src="../assets/images/bg-1.png" alt="" class="w-100 rounded-2">
-              <div class="d-flex mt-2 align-items-center">
-                <i class='bx bxs-school fs-2'></i>
-                <p class="m-0 ms-3 fs-4">Campus A</p>
-              </div>
-              <div class="d-flex mt-2 align-items-center">
-                <i class='bx bxs-map fs-2'></i>
-                <p class="m-0 ms-3 fs-4 text-start">Location</p>
+              <div class="details mx-3">
+                <div class="d-flex mt-2 align-items-center">
+                  <i class='bx bxs-school fs-2'></i>
+                  <p class="m-0 ms-3 fs-4">Campus A</p>
+                </div>
+                <div class="d-flex mt-2 align-items-center">
+                  <i class='bx bxs-map fs-2'></i>
+                  <p class="m-0 ms-3 fs-4 text-start">Location</p>
+                </div>
               </div>
             </div>  
           </div>
@@ -37,17 +39,19 @@
       </div>
 
       <div class="col p-0 mb-3">
-        <a href="#" class="card mx-3 mb-sm-3 rounded-3 shadow-sm h-100">
+        <a href="./view_campus" class="card mx-3 mb-sm-3 rounded-3 shadow-sm h-100">
           <div class="card-body d-flex flex-column justify-content-between shadow-sm">
             <div class="align-content-center text-center">
               <img src="../assets/images/bg-1.png" alt="" class="w-100 rounded-2">
-              <div class="d-flex mt-2 align-items-center">
-                <i class='bx bxs-school fs-2'></i>
-                <p class="m-0 ms-3 fs-4">Campus B</p>
-              </div>
-              <div class="d-flex mt-2 align-items-center">
-                <i class='bx bxs-map fs-2'></i>
-                <p class="m-0 ms-3 fs-4 text-start">Location</p>
+              <div class="details mx-3">
+                <div class="d-flex mt-2 align-items-center">
+                  <i class='bx bxs-school fs-2'></i>
+                  <p class="m-0 ms-3 fs-4">Campus A</p>
+                </div>
+                <div class="d-flex mt-2 align-items-center">
+                  <i class='bx bxs-map fs-2'></i>
+                  <p class="m-0 ms-3 fs-4 text-start">Location</p>
+                </div>
               </div>
             </div>  
           </div>
@@ -55,22 +59,25 @@
       </div>
 
       <div class="col p-0 mb-3">
-        <a href="#" class="card mx-3 mb-sm-3 rounded-3 shadow-sm h-100">
+        <a href="./view_campus" class="card mx-3 mb-sm-3 rounded-3 shadow-sm h-100">
           <div class="card-body d-flex flex-column justify-content-between shadow-sm">
             <div class="align-content-center text-center">
               <img src="../assets/images/bg-1.png" alt="" class="w-100 rounded-2">
-              <div class="d-flex mt-2 align-items-center">
-                <i class='bx bxs-school fs-2'></i>
-                <p class="m-0 ms-3 fs-4">Campus C</p>
-              </div>
-              <div class="d-flex mt-2 align-items-center">
-                <i class='bx bxs-map fs-2'></i>
-                <p class="m-0 ms-3 fs-4 text-start">Location</p>
+              <div class="details mx-3">
+                <div class="d-flex mt-2 align-items-center">
+                  <i class='bx bxs-school fs-2'></i>
+                  <p class="m-0 ms-3 fs-4">Campus A</p>
+                </div>
+                <div class="d-flex mt-2 align-items-center">
+                  <i class='bx bxs-map fs-2'></i>
+                  <p class="m-0 ms-3 fs-4 text-start">Location</p>
+                </div>
               </div>
             </div>  
           </div>
         </a>
       </div>
+
     </div>
   </section>
 
