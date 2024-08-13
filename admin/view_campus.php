6dfafd306fe089a6f14393a88508791808cb4d70
Frankii <franklinoliveros2003@gmail.com>
2024-08-13 12:52:28 +0800

    update view campus

--- admin/view_campus.php
+++ admin/view_campus.php
@@ -1,5 +1,5 @@
 <html lang="en">
-<?php 
+<?php
   $title = 'Campuses | Campus A';
 	include './includes/admin_head.php';
   function getCurrentPage() {
@@ -7,10 +7,10 @@
   }
 ?>
 <body>
-  <?php 
+  <?php
     require_once ('./includes/admin_header.php');
   ?>
-  <?php 
+  <?php
     require_once ('./includes/admin_sidepanel.php');
   ?>
 
@@ -21,39 +21,35 @@
       <img src="../assets/images/bg-1.png" alt="" class="w-75 rounded-2">
     </div>
 
-    <div class="campus_name text-center my-2">
+    <div class="campus_name text-center mt-2 mb-3">
       <h2>Campus A</h2>
     </div>
 
-    <div class="row row-cols-1 row-cols-md-2 mx-1 mx-md-5">
-      <div class="col d-flex">
-        <i class='bx bx-map'></i> <!-- Address Icon -->
-        <p>Location</p>
+    <div class="row row-cols-1 row-cols-md-2 mx-1 mx-md-5 justify-content-center">
+      <div class="col d-flex flex-column align-items-center justify-content-center border-bottom border-2">
+        <i class='bx bx-map fs-3 text-danger'></i> <!-- Address Icon -->
+        <p class="fs-5 mb-2">Location</p>
+      </div>
+      <div class="col d-flex flex-column align-items-center justify-content-center border-bottom border-2">
+        <i class='bx bx-phone fs-3 text-danger'></i> <!-- Contact Number Icon -->
+        <p class="fs-5 mb-2">+63 9xx xxx xxxx</p>
+      </div>
+      <div class="col d-flex flex-column align-items-center justify-content-center border-bottom border-2">
+        <i class='bx bx-envelope fs-3 text-danger'></i> <!-- Email Icon -->
+        <p class="fs-5 mb-2">example@email.com</p>
+      </div>
+      <!-- <div class="col">
+        -- Facilities Icon --
+        <i class='bx bx-building'></i>
       </div>
       <div class="col">
-        <i class='bx bx-phone'></i> <!-- Contact Number Icon -->
-      </div>
-      <div class="col">
-        <i class='bx bx-envelope'></i> <!-- Email Icon -->
-      </div>
-      <div class="col">
-        <i class='bx bx-map-alt'></i> <!-- Campus Map Icon -->
-      </div>
-      <div class="col">
-        <i class='bx bx-building'></i> <!-- Facilities Icon -->
-      </div>
-      <div class="col">
-        <i class='bx bx-book-content'></i> <!-- Programs Offered Icon -->
-      </div>
+        -- Programs Offered Icon --
+        <i class='bx bx-book-content'></i>
+      </div> -->
     </div>
-
-    <i class='bx bx-map'></i> <!-- Address Icon -->
-    <i class='bx bx-phone'></i> <!-- Contact Number Icon -->
-    <i class='bx bx-envelope'></i> <!-- Email Icon -->
-    <i class='bx bx-map-alt'></i> <!-- Campus Map Icon -->
-    <i class='bx bx-building'></i> <!-- Facilities Icon -->
-    <i class='bx bx-book-content'></i> <!-- Programs Offered Icon -->
-
+    <div class="col d-flex flex-column align-items-center justify-content-center border-bottom border-2">
+      <i class='bx bx-map-alt fs-3 text-danger'></i> <!-- Campus Map Icon -->
+    </div>
   </section>
 
 </body>
