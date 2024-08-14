79b05a76ad520e33f09456b916bbac2878651f2e
Frankii <franklinoliveros2003@gmail.com>
2024-08-14 20:32:03 +0800

    Revert "add/ edit campus"
    This reverts commit a576c864866c571087a5c333a06104991122cecd.

--- admin/add_campus.php
+++ admin/add_campus.php
@@ -20,45 +20,39 @@
       <div class="col-2"></div>
 
       <div class="col-8">
-      <form>
-  <div class="border shadow p-3 mb-5 bg-body rounded">
-    <h3 class="text-center">Add Campus</h3>
-    <div class="user">
-      <div class="campus-pic">
-        <label class="label brand-border-color d-flex flex-column" for="file" style="border-width: 4px !important;">
-          <i class="bx bxs-camera-plus"></i>
-          <span>Change Image</span>
-        </label>
-        <img src="../assets/images/bg-1.png" id="output" class="img-fluid rounded-3 w-75">
-        <input id="file" type="file" name="profile" accept="image/png, image/jpeg" onchange="validateFile(event)">
-      </div>
-    </div>
-
-    <div class="form-group mb-2">
-      <label for="name">School Name</label>
-      <input type="text" class="form-control" id="name" placeholder="campus name">
-    </div>
-    <div class="form-group mb-2">
-      <label for="address">Address</label>
-      <input type="text" class="form-control" id="address" placeholder="address">
-    </div>
-    <div class="form-group mb-2">
-      <label for="contact">School Phone</label>
-      <input type="text" class="form-control" id="contact" placeholder="+63 9xx xxx xxxx">
-    </div>
-    <div class="form-group">
-      <label for="email">Email address</label>
-      <input type="email" class="form-control" id="email" placeholder="name@example.com">
-    </div>
-
-    <!-- Save and Cancel Buttons -->
-    <div class="d-flex justify-content-end mt-3">
-      <a href="./campus" class="btn btn-secondary me-2 link-light">Cancel</a>
-      <button type="submit" class="btn btn-primary link-light">Save</button>
-    </div>
-  </div>
-</form>
-
+        <form>
+          <div class="border shadow p-3 mb-5 bg-body rounded">
+            <div class="user">
+              <div class="profile-pic">
+                <label class="label brand-border-color d-flex flex-column" for="file" style="border-width: 4px !important;">
+                  <i class="bx bxs-camera-plus"></i>
+                  <span>Change Image</span>
+                </label>
+                <img src="../assets/images/bg-1.png" id="output" class="img-fluid">
+                <input id="file" type="file" name="profile" accept="image/png, image/jpeg" onchange="validateFile(event)">
+              </div>
+              <div class="d-flex justify-content-center align-items-center mb-2">
+                <button type="submit" name="saveimage" class="btn brand-bg-color">Save Image</button>
+              </div>
+            </div>
+            <div class="form-group mb-2">
+              <label for="name">School Name</label>
+              <input type="text" class="form-control" id="name" placeholder="campus name">
+            </div>
+            <div class="form-group mb-2">
+              <label for="address">Address</label>
+              <input type="text" class="form-control" id="address" placeholder="address">
+            </div>
+            <div class="form-group mb-2">
+              <label for="contact">School Phone</label>
+              <input type="text" class="form-control" id="contact" placeholder="+63 9xx xxx xxxx">
+            </div>
+            <div class="form-group">
+              <label for="email">Email address</label>
+              <input type="email" class="form-control" id="email" placeholder="name@example.com">
+            </div>
+          </div>
+        </form>
       </div>
 
       <div class="col-2"></div>
