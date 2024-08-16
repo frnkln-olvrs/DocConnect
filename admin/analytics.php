370c549c9b45b233a2bdfc3df2b9453e4a5b354a
Frankii <franklinoliveros2003@gmail.com>
2024-08-16 23:01:13 +0800

    donut chart update

--- admin/analytics.php
+++ admin/analytics.php
@@ -46,38 +46,7 @@
   </section>
 
   <script src="./js/analytics-lineChart.js"></script>
-  <script>
-    const doughnutData = {
-      labels: ['Red', 'Blue', 'Yellow'],
-      datasets: [{
-        label: 'My First Dataset',
-        data: [300, 50, 100],
-        backgroundColor: [
-          'rgb(255, 99, 132)',
-          'rgb(54, 162, 235)',
-          'rgb(255, 205, 86)'
-        ],
-        hoverOffset: 4
-      }]
-    };
-  
-    const doughnutCtx = document.getElementById('doughnutChart').getContext('2d');
-    const doughnutChart = new Chart(doughnutCtx, {
-      type: 'doughnut',
-      data: doughnutData,
-      options: {
-        legend: {
-          display: true
-        },
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    });
-  </script>
-
+  <script src="./js/analytics-donutChart.js"></script>
 
 </body>
 </html>
--- admin/js/analytics-donutChart.js
+++ admin/js/analytics-donutChart.js
@@ -1,29 +1,29 @@
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-const data = {
-  labels: labels,
+const doughnutData = {
+  labels: ['Red', 'Blue', 'Yellow'],
   datasets: [{
     label: 'My First Dataset',
-    data: [65, 59, 80, 81, 56, 55, 40],
+    data: [300, 50, 100],
     backgroundColor: [
       'rgb(255, 99, 132)',
       'rgb(54, 162, 235)',
-      'rgb(255, 205, 86)',
-      'rgb(75, 192, 192)',
-      'rgb(153, 102, 255)',
-      'rgb(255, 159, 64)',
-      'rgb(201, 203, 207)'
+      'rgb(255, 205, 86)'
     ],
     hoverOffset: 4
   }]
 };
 
-const config = {
+const doughnutCtx = document.getElementById('doughnutChart').getContext('2d');
+const doughnutChart = new Chart(doughnutCtx, {
   type: 'doughnut',
-  data: data,
-};
-
-const myDoughnutChart = new Chart(
-  document.getElementById('myDonutChart'),
-  config
-);
+  data: doughnutData,
+  options: {
+    legend: {
+      display: true
+    },
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
