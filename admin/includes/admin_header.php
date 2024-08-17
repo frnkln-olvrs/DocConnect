5e95b0e0f920e5c275c349e8feb376ffeb0bd66d
Frankii <franklinoliveros2003@gmail.com>
2024-08-17 18:55:48 +0800

    notification?

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,23 +1,54 @@
 <header class="p-3 border-bottom bg-green shadow-sm position-fixed w-100 z-3 border-bottom-0">
   <div class="mx-1 mx-lg-3">
     <div class="d-flex flex-wrap justify-content-between">
-    <a class="d-flex align-items-center text-dark text-decoration-none" href="./index">
-      <img src="../assets/images/logo.png" class="me-2" alt="Logo" height="32">
-      <h1 class="fs-4 link-light m-0 d-name">Doc<span class="link-light">Connect</span></h1>
-		</a>
+      <a class="d-flex align-items-center text-dark text-decoration-none" href="./index">
+        <img src="../assets/images/logo.png" class="me-2" alt="Logo" height="32">
+        <h1 class="fs-4 link-light m-0 d-name">Doc<span class="link-light">Connect</span></h1>
+		  </a>
 
-      <div class="dropdown text-end">
-        <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-          <img src="./../assets/images/profile_img.jpg" alt="mdo" width="32" height="32" class="rounded-circle">
-        </a>
-        <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
-          <li><a class="dropdown-item" href="#">New project...</a></li>
-          <li><a class="dropdown-item" href="#">Settings</a></li>
-          <li><a class="dropdown-item" href="#">Profile</a></li>
-          <li><hr class="dropdown-divider"></li>
-          <li><a class="dropdown-item" href="#">Sign out</a></li>
-        </ul>
+      <div class="d-flex">
+        <div class="dropdown me-3">
+          <a href="#" class="dropdown-toggle text-decoration-none text-dark" id="notificationDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="far fa-bell fa-lg"></i>
+            <i class='bx bx-bell'></i>
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end p-2" aria-labelledby="notificationDropdown">
+            <li class="dropdown-item d-flex align-items-center">
+              <span class="icon me-2"></span>
+              <div>
+                <div class="fw-bold">Lorem, ipsum dolor.</div>
+                <small class="text-muted">Lorem ipsum dolor sit amet consectetur.</small>
+              </div>
+              <div class="ms-auto text-success">Success</div>
+            </li>
+            <li class="dropdown-item d-flex align-items-center">
+              <span class="icon me-2"></span>
+              <div>
+                <div class="fw-bold">Lorem, ipsum dolor.</div>
+                <small class="text-muted">Lorem ipsum dolor sit amet consectetur.</small>
+              </div>
+              <div class="ms-auto text-danger">Failed</div>
+            </li>
+            <!-- Add more notifications as needed -->
+            <li class="dropdown-item text-center fw-bold text-primary">
+              <a href="#">Show All Activities</a>
+            </li>
+          </ul>
+        </div>
+        <div class="dropdown text-end">
+          <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="./../assets/images/profile_img.jpg" alt="mdo" width="32" height="32" class="rounded-circle">
+          </a>
+          <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
+            <li><a class="dropdown-item" href="#">New project...</a></li>
+            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Sign out</a></li>
+          </ul>
+        </div>
       </div>
+
     </div>
   </div>
 </header>
