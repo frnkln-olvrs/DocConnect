d3dd0a0ba4c4bcd585661dd14299cd36f8318ba8
Frankii <franklinoliveros2003@gmail.com>
2024-08-19 15:24:15 +0800

    Update analytics.php

--- admin/analytics.php
+++ admin/analytics.php
@@ -14,22 +14,23 @@
     require_once ('./includes/admin_sidepanel.php');
   ?>
 
-<section id="analytics">
-    <div class="row row-cols-1 row-cols-md-2">
-      <div class="col">
+  <section id="analytics">
+    <div class="row mx-4">
+      <!-- First Column -->
+      <div class="col-12 col-md-6 d-flex flex-column justify-content-center py-2 px-0 border border-danger-subtle shadow-lg">
         <nav>
           <div class="nav nav-tabs mb-3" id="nav-tab" role="tablist">
-            <button class="nav-link  active" id="nav-campus-tab" data-bs-toggle="tab" data-bs-target="#nav-campus" type="button" role="tab" aria-controls="nav-campus" aria-selected="true">Campus</button>
+            <button class="nav-link active" id="nav-campus-tab" data-bs-toggle="tab" data-bs-target="#nav-campus" type="button" role="tab" aria-controls="nav-campus" aria-selected="true">Campus</button>
             <button class="nav-link" id="nav-type-tab" data-bs-toggle="tab" data-bs-target="#nav-type" type="button" role="tab" aria-controls="nav-type" aria-selected="false">Type</button>
           </div>
         </nav>
-        <div class="tab-content p-3 border bg-white" id="nav-tabContent">
+        <div class="tab-content p-3 mx-2 border bg-white" id="nav-tabContent">
           <select id="yearSelect" class="form-select form-select-sm w-25" aria-label=".form-select-sm example">
             <option value="1">2021-2022</option>
             <option value="2">2022-2023</option>
             <option value="3">2023-2024</option>
           </select>
-
+    
           <div class="tab-pane fade active show" id="nav-campus" role="tabpanel" aria-labelledby="nav-campus-tab">
             <canvas id="campusChart" class="chart" role="img"></canvas>
           </div>
@@ -38,9 +39,13 @@
           </div>
         </div>
       </div>
-
-      <div class="col">
-        <canvas id="doughnutChart" class="chart" role="img"></canvas>
+    
+      <!-- Spacer Column -->
+      <div class="col-12 col-md-1 p-0 m-0"></div>
+    
+      <!-- Second Column -->
+      <div class="col-12 col-md-5 d-flex justify-content-center p-3 border border-danger-subtle shadow-lg">
+        <canvas id="doughnutChart" class="chart" role="img" style="max-height: 451px;"></canvas>
       </div>
     </div>
   </section>
