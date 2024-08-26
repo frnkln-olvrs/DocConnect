3c02e699b8ce30d8e2eec82541dc30bc447cfb58
Frankii <franklinoliveros2003@gmail.com>
2024-08-26 23:01:16 +0800

    Update doctor.signup.php

--- doctor/doctor.signup.php
+++ doctor/doctor.signup.php
@@ -5,7 +5,7 @@
   include '../includes/head.php';
 ?>
 <body class="bg-green d-flex align-items-center min-vh-100">
-  <div class="container mt-5">
+  <div class="container">
     <div class="row justify-content-center">
       <div class="col-12 col-md-6">
         <div class="card shadow-lg">
@@ -15,22 +15,37 @@
           
           <div class="card-body">
             <form>
+              <div class="row">
+                <div class="col-12 col-md-4 mb-3">
+                  <label for="f_name" class="form-label">First Name</label>
+                  <input type="text" class="form-control" id="f_name" placeholder="first name" required>
+                </div>
+                <div class="col-12 col-md-4 mb-3">
+                  <label for="m_name" class="form-label">Middle Name</label>
+                  <input type="text" class="form-control" id="m_name" placeholder="middle name">
+                </div>
+                <div class="col-12 col-md-4 mb-3">
+                  <label for="l_name" class="form-label">Last Name</label>
+                  <input type="text" class="form-control" id="l_name" placeholder="last name" required>
+                </div>
+              </div>
 
-            <div class="row">
-              <div class="col-12 col-md-4 mb-3">
-                <label for="f_name" class="form-label">First Name</label>
-                <input type="text" class="form-control" id="f_name" placeholder="first name" required>
+              <div class="row">
+                <div class="col-12 col-md-6 mb-3">
+                  <label for="gender" class="form-label">Gender</label>
+                  <select id="gender" class="form-select" required>
+                    <option value="" disabled selected>Select gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                  </select>
+                </div>
+                <div class="col-12 col-md-6 mb-3">
+                  <label for="birthday" class="form-label">Birthday</label>
+                  <input type="date" class="form-control" id="birthday" required>
+                </div>
               </div>
-              <div class="col-12 col-md-4 mb-3">
-                <label for="m_name" class="form-label">Middle Name</label>
-                <input type="text" class="form-control" id="m_name" placeholder="middle name" >
-              </div>
-              <div class="col-12 col-md-4 mb-3">
-                <label for="l_name" class="form-label">Last Name</label>
-                <input type="text" class="form-control" id="l_name" placeholder="last name" required>
-              </div>
-            </div>
-            
+
               <div class="mb-3">
                 <label for="email" class="form-label">Email address</label>
                 <input type="email" class="form-control" id="email" placeholder="Enter your email" required>
