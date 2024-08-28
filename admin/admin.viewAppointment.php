b0a5f6655470c262fdc8d683b1ac9e8a12b4ec1a
Frankii <franklinoliveros2003@gmail.com>
2024-08-28 17:59:46 +0800

    Update admin.viewAppointment.php

--- admin/admin.viewAppointment.php
+++ admin/admin.viewAppointment.php
@@ -19,14 +19,56 @@
 
       <div class="col-2"></div>
 
-      <div class="col-8">
+      <div class="col-12 col-lg-8">
         <form>
-          <div class="border shadow p-3 mb-5 bg-body rounded">
+          <div class="border shadow p-4 mb-5 bg-body rounded">
             <h5 class="text-center">Status: <span class="text-success"> Completed</span></h5>
 
-            <hr class="mx-3 my-2">
+            <hr class="mx-3 my-4">
 
+            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+              <div class="col d-flex ">
+                <strong class="me-2">Code:</strong>
+                <p>001</p>
+              </div>
 
+              <div class="col d-flex ">
+                <strong class="me-2">Type:</strong>
+                <p>Face to face</p>
+              </div>
+
+              <div class="col d-flex ">
+                <strong class="me-2">Date:</strong>
+                <p>Monday, 9:00am - 10:00pm</p>
+              </div>
+
+              <div class="col d-flex ">
+                <strong class="me-2">Campus:</strong>
+                <p>Campus B</p>
+              </div>
+
+              <div class="col d-flex ">
+                <strong class="me-2">Address:</strong>
+                <p>Zambnonaga City</p>
+              </div>
+
+              <div class="col d-flex ">
+                <strong class="me-2">Patient:</strong>
+                <p>Allen Barry</p>
+              </div>
+
+              <div class="col d-flex ">
+                <strong class="me-2">Email:</strong>
+                <p>username123@email.com</p>
+              </div>
+
+              <div class="col d-flex ">
+                <strong class="me-2">phone:</strong>
+                <p>+63 9xx xxx xxxx</p>
+              </div>
+            </div>
+
+            
 
           </div>
         </form>
