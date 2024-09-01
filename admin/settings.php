4f3c5044b999629ee21391aaa27eee856f8c764e
Frankii <franklinoliveros2003@gmail.com>
2024-09-01 13:11:18 +0800

    Create settings.php

---
+++ admin/settings.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<?php 
+  $title = 'Admin | Settings';
+  include './includes/admin_head.php';
+  function getCurrentPage() {
+    return basename($_SERVER['PHP_SELF']);
+  }
+?>
+<body>
+  <?php 
+    require_once ('./includes/admin_header.php');
+  ?>
+  <?php 
+    require_once ('./includes/admin_sidepanel.php');
+  ?>
+
+  <section id="dashboard" class="page-container">
+    <h1 class="text-center">Overview</h1>
+
+    
+
+</body>
+</html>
