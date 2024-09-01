802240548359406f36d1fcb481ee7a5be6c53013
mrhiraru <mrhiraru@gmail.com>
2024-09-01 15:15:42 +0800

    232

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
-<?php 
+<?php
 session_start();
 session_destroy();
-
+header('location: ./user/login.php');
 ?>
--- user/verification.php
+++ user/verification.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 
-// if (isset($_SESSION['user_role']) && $_SESSION['verification_status'] == "Verified") {
-//     header('location: ../index.php');
-// } else if (!isset($_SESSION['user_role'])) {
-//     header('location: ./login.php');
-// }
+if ((isset($_SESSION['user_role']) &&  $_SESSION['user_role'] == 0) && $_SESSION['verification_status'] == "Verified") {
+  header('location: ../admin/index.php');
+} else if (!isset($_SESSION['user_role'])) {
+  header('location: ./login.php');
+}
 
 require_once("../classes/account.class.php");
 require_once("../tools/functions.php");
@@ -15,42 +15,44 @@ require_once("../tools/mailer.php");
 $verification_code = generate_code();
 
 if (!isset($_SESSION['code'])) {
-    $_SESSION['code'] = $verification_code;
-    send_code($_SESSION['email'], $_SESSION['fullname'], $_SESSION['code']);
+  $_SESSION['code'] = $verification_code;
+  send_code($_SESSION['email'], $_SESSION['fullname'], $_SESSION['code']);
 } else if (isset($_POST['resend'])) {
-    $_SESSION['code'] = $verification_code;
-    send_code($_SESSION['email'], $_SESSION['fullname'], $_SESSION['code']);
+  $_SESSION['code'] = $verification_code;
+  send_code($_SESSION['email'], $_SESSION['fullname'], $_SESSION['code']);
 } else if (isset($_POST['verify'])) {
-    $account = new Account();
+  $account = new Account();
 
-    $account->verification_status = 'Verified';
-    $account->account_id = $_SESSION['account_id'];
-    $code = htmlentities($_POST['code']);
+  $account->verification_status = 'Verified';
+  $account->account_id = $_SESSION['account_id'];
+  $code = htmlentities($_POST['code']);
 
-    if ($code == $_SESSION['code'] && validate_field($code)) {
-        if ($account->verify()) {
-            $_SESSION['verification_status'] = 'Verified';
-            $success = 'success';
-        } else {
-            echo 'An error occured while updating in the database.';
-        }
+  if ($code == $_SESSION['code'] && validate_field($code)) {
+    if ($account->verify()) {
+      $_SESSION['verification_status'] = 'Verified';
+      $success = 'success';
     } else {
-        $error = 'Invalid verification code.';
-        $success = 'failed';
+      echo 'An error occured while updating in the database.';
     }
+  } else {
+    $error = 'Invalid verification code.';
+    $success = 'failed';
+  }
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<?php 
-  $title = 'Admin | Verify';
-  include '../admin/includes/admin_head.php';
-  function getCurrentPage() {
-    return basename($_SERVER['PHP_SELF']);
-  }
+<?php
+$title = 'Admin | Verify';
+include '../admin/includes/admin_head.php';
+function getCurrentPage()
+{
+  return basename($_SERVER['PHP_SELF']);
+}
 ?>
+
 <body class="bg-danger d-flex align-items-center min-vh-100">
   <div class="container">
     <div class="row justify-content-center">
@@ -59,23 +61,23 @@ if (!isset($_SESSION['code'])) {
           <div class="card-header text-center">
             <h4>Verify</h4>
           </div>
-          
+
           <div class="card-body">
             <form method="post" action="">
               <div class="mb-3">
                 <label for="verify" class="form-label">Verification Code</label>
-                <input type="text" class="form-control" id="verify" placeholder="Enter code" required>
+                <input type="text" class="form-control" id="verify" name="code" placeholder="Enter code" required>
                 <?php
-                    if (isset($_POST['code']) && !validate_field($_POST['code']) && isset($_POST['verify'])) {
-                    ?>
-                        <p class="text-dark m-0 ps-2">Verification code is required.</p>
-                    <?php
-                    } else if (isset($_POST['code']) && ($_POST['code'] != $_SESSION['code']) && isset($error)) {
-                    ?>
-                        <p class="text-dark m-0 ps-2"><?= $error ?></p>
-                    <?php
-                    }
-                    ?>
+                if (isset($_POST['code']) && !validate_field($_POST['code']) && isset($_POST['verify'])) {
+                ?>
+                  <p class="text-dark m-0 ps-2">Verification code is required.</p>
+                <?php
+                } else if (isset($_POST['code']) && ($_POST['code'] != $_SESSION['code']) && isset($error)) {
+                ?>
+                  <p class="text-dark m-0 ps-2"><?= $error ?></p>
+                <?php
+                }
+                ?>
               </div>
               <div class="mb-3 text-end">
                 <input type="submit" id="resend-link" class="text-primary bg-white border-0" name="resend" value="Resend">
@@ -98,7 +100,7 @@ if (!isset($_SESSION['code'])) {
       var countdownElement = document.getElementById("countdown");
       var resendLink = document.getElementById("resend-link");
       var countdownTime = 60;
-      
+
       function startCountdown() {
         var countdownInterval = setInterval(function() {
           countdownElement.innerText = "[" + countdownTime + "]";
@@ -121,4 +123,5 @@ if (!isset($_SESSION['code'])) {
     });
   </script>
 </body>
+
 </html>
