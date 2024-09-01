b3bad64e159dd02b91c94f0fc39aee77d144403e
Frankii <franklinoliveros2003@gmail.com>
2024-09-01 14:21:41 +0800

    on off toggle
    css html

--- admin/settings.php
+++ admin/settings.php
@@ -6,6 +6,8 @@
     return basename($_SERVER['PHP_SELF']);
   }
 ?>
+
+<link rel="stylesheet" href="./css/OnOffToggle.css">
 <body>
   <?php 
     require_once ('./includes/admin_header.php');
@@ -15,9 +17,56 @@
   ?>
 
   <section id="dashboard" class="page-container">
-    <h1 class="text-center">Overview</h1>
+    <h1 class="text-start">User Group Management</h1>
 
-    
+    <!------ Include the above in your HEAD tag ---------->
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-6">
+      <h2>Wired style</h2>
+      <div>
+        <label class="switch">
+            <input type="checkbox">
+            <span class="slider"></span>
+        </label> Switch1
+      </div>
+      <div>
+        <label class="switch">
+            <input type="checkbox">
+            <span class="slider"></span>
+        </label> Switch2
+      </div>
+      <div>
+        <label class="switch">
+            <input type="checkbox">
+            <span class="slider"></span>
+        </label> Switch3
+      </div>
+    </div></col-->
+    <div class="col-sm-6">
+      <h2>Flat style</h2>
+      <div>
+        <label class="switch flat">
+            <input type="checkbox">
+            <span class="slider"></span>
+        </label> Switch1
+      </div>
+      <div>
+        <label class="switch flat">
+            <input type="checkbox">
+            <span class="slider"></span>
+        </label> Switch2
+      </div>
+      <div>
+        <label class="switch flat">
+            <input type="checkbox">
+            <span class="slider"></span>
+        </label> Switch3
+      </div>
+    </div></col-->
+  </div></row-->
+</div></container-->
 
 </body>
 </html>
