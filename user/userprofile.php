92e3b870bf1e36b1db5677c50b50e5371552619c
Frankii <franklinoliveros2003@gmail.com>
2024-09-06 13:30:26 +0800

    123

--- user/userprofile.php
+++ user/userprofile.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php 
-  $title = 'DocConnect';
+  $title = 'DocConnect | Profile';
 	include '../includes/head.php';
 ?>
 <body>
   <?php 
     require_once ('../includes/header.php');
   ?>
+
+  <section class="vh-100">
+    <div class="row row-cols-1 row-cols-md-2">
+      <div class="col">
+        asdasd
+      </div>
+      <div class="col">
+        jajksdklajsd
+      </div>
+    </div>
+  </section>
   
 
   <?php 
