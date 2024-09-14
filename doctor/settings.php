6f7ae7dedd11c005b5cc4ba7234b0b37c5f036fb
Frankii <franklinoliveros2003@gmail.com>
2024-09-14 23:12:26 +0800

    Update settings.php

---
+++ doctor/settings.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$title = 'Settings';
+include '../includes/head.php';
+?>
+
+<body>
+  <?php
+    require_once('../includes/header-doctor.php');
+  ?>
+
+  <div class="container-fluid">
+    <div class="row">
+      <?php 
+        require_once('../includes/sidepanel-doctor.php');
+      ?>
+      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <h2 class="text-start my-2">Account settings</h2>
+
+        <div class="row">
+          <div class="col-4">asd</div>
+          <div class="col-8">asjdlaks</div>
+        </div>
+      </main>
+    </div>
+  </div>
+</body>
