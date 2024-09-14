2ce4f88d0ac1d2e83bda7dc4ff70206675ada91a
Frankii <franklinoliveros2003@gmail.com>
2024-09-14 23:55:15 +0800

    pape js animation

--- login.php
+++ login.php
@@ -114,22 +114,20 @@
   	</div>
 
   	<div class="overlay-container">
-  		<div class="overlay">
-
-  			<div class="overlay-panel overlay-left">
-  				<h1>Welcome Back!</h1>
-  				<p>To keep connected with us please login with your personal info</p>
-  				<button class="ghost" id="signIn-overlay">Sign In</button>
-  			</div>
-
-  			<div class="overlay-panel overlay-right">
-  				<h1>Hello, Friend!</h1>
-  				<p>Enter your personal details and start journey with us</p>
-  				<button class="ghost" id="signUp-overlay">Sign Up</button>
-  			</div>
-
-  		</div>
-  	</div>
+      <canvas id="canvas" class="canvas-back"></canvas>
+      <div class="overlay">
+        <div class="overlay-panel overlay-left">
+          <h1>Welcome Back!</h1>
+          <p>To keep connected with us please login with your personal info</p>
+          <button class="ghost" id="signIn-overlay">Sign In</button>
+        </div>
+        <div class="overlay-panel overlay-right">
+          <h1>Hello, Friend!</h1>
+          <p>Enter your personal details and start journey with us</p>
+          <button class="ghost" id="signUp-overlay">Sign Up</button>
+        </div>
+      </div>
+    </div>
   </div>
 
   <script src="./js/login.js"></script>
