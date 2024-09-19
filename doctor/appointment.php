50e1412ae0164230d9c4902198cbbd45a5d1649d
Frankii <franklinoliveros2003@gmail.com>
2024-09-19 19:13:17 +0800

    Create appointment.php

---
+++ doctor/appointment.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$title = 'Appointment';
+$dashboard = 'active';
+include '../includes/head.php';
+?>
+
+<body>
+  <?php
+  require_once('../includes/header-doctor.php');
+  ?>
+  <div class="container-fluid">
+    <div class="row">
+      <?php
+      require_once('../includes/sidepanel-doctor.php');
+      ?>
+      <main class="col-md-9 ms-sm-auto col-lg-10 bg-light">
+        <div class="container my-4">
+          asdad
+        </div>
+      </main>
+    </div>
+  </div>
+
+</body>
+
+</html>
