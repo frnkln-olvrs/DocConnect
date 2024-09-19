e8c0986da404141797e06950cd1710779bdb6d97
Frankii <franklinoliveros2003@gmail.com>
2024-09-19 17:04:26 +0800

    Update sidepanel-doctor.php

--- includes/sidepanel-doctor.php
+++ includes/sidepanel-doctor.php
@@ -1,10 +1,13 @@
 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block border-end sidebar collapse min-vh-100 p-0">
     <div class="position-sticky pt-3">
-        <div class="pt-1 pb-2 mb-2 border-bottom d-flex align-items-center flex-column">
+        <div class="pt-1 mb-2 d-flex align-items-center flex-column">
             <img src="../assets/images/defualt_profile.png" alt="" height="100" width="100" class="rounded rounded-circle border border-2 border-primary mb-2">
-            <p class="h5 text-dark fw-semibold">Dr. John Doe</p>
-            <p class="h6 text-secondary">Medical Practitioner</p>
+            <h5 class="text-primary fw-semibold mb-1">Dr. John Doe</h5>
+            <p class="text-muted fw-light mb-0">Medical Practitioner</p>
         </div>
+
+        <hr class="m-0 mb-2 mx-3 border-primary">
+
         <ul class="nav doctors_nav flex-column">
             <li class="nav-item">
                 <a class="nav-link text-dark <?= $dashboard ?>" href="../doctor/index">
@@ -16,6 +19,12 @@
                 <a class="nav-link text-dark <?= $profile ?>" href="../doctor/profile">
                     <i class='bx bxs-user-detail me-2'></i>
                     Profile
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-dark <?= $appointment ?>" href="../doctor/appointment">
+                    <i class='bx bx-book-bookmark me-2'></i>
+                    Appointment
                 </a>
             </li>
             <li class="nav-item">
