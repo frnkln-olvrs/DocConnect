4afa93fffc8d6695fa64cd15fe6fab8eca33005b
Frankii <franklinoliveros2003@gmail.com>
2025-01-04 23:01:32 +0800

    Update profile_appointment.php
    modal

--- user/profile_appointment.php
+++ user/profile_appointment.php
@@ -39,7 +39,7 @@ require_once('../classes/account.class.php');
             <div class="card-body">
               <div class="row m-0" id="calendar-container">
                 <div class="col-md-12">
-                  <h5 class="card-title mb-2">Calendar</h5>
+                  <h5 class="card-title mb-2 text-green">Calendar</h5>
                   <div id="calendar" class="m-0"></div>
                 </div>
               </div>
@@ -48,14 +48,94 @@ require_once('../classes/account.class.php');
 
           <div class="card bg-body-tertiary mb-4">
             <div class="card-body">
-
+              <h5 class="card-title mb-2 text-green">January List</h5>
+              <hr>
+              <div class="table-responsive">
+              <table class="table table-striped" id="eventsTable">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Date & Time</th>
+                    <th>Doctor</th>
+                    <th>Department</th>
+                    <th>Link</th>
+                    <th>Status</th>
+                    <th class="text-center">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>01-01-25 | 10:00am</td>
+                      <td>Dr. Smith</td>
+                      <td>Cardiology</td>
+                      <td><a href="#" class="btn btn-success btn-sm"><i class='bx bx-video text-light'></i></td>
+                      <td>Confirmed</td>
+                      <td class="text-center">
+                        <button id="addAppointmentBtn" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#addEventModal"><i class='bx bx-show text-light'></i></button>
+                        <button class="btn btn-danger btn-sm ms-2"><i class='bx bxs-trash text-light'></i></button>
+                      </td>
+                    </tr>
+                </tbody>
+              </table>
+            </div>
             </div>
           </div>
-
         </div>
       </div>
     </div>
   </section>
+
+  <!-- Bootstrap Modal for Adding Events -->
+  <div class="modal fade" id="addEventModal" tabindex="-1" aria-labelledby="addEventModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="addEventModalLabel">Add Appointment</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form id="addEventForm">
+            <div class="mb-3">
+              <label for="eventTitle" class="form-label">Event Title</label>
+              <input type="text" class="form-control" id="eventTitle" name="eventTitle" required>
+            </div>
+            <div class="mb-3">
+              <label for="eventDate" class="form-label">Event Date</label>
+              <input type="date" class="form-control" id="eventDate" name="eventDate" required>
+            </div>
+            <div class="d-flex align-items-center w-100">
+              <div class="mb-3 w-100">
+                <label for="startTime" class="form-label">Time Start</label>
+                <input type="time" class="form-control" id="startTime" name="startTime" required>
+              </div>
+              <p class="m-0 mx-3 mt-3"> to </p>
+              <div class="mb-3 w-100">
+                <label for="endTime" class="form-label">Time End</label>
+                <input type="time" class="form-control" id="endTime" name="endTime">
+              </div>
+            </div>
+            <div class="mb-3">
+              <label class="form-label">Meeting Type</label>
+              <div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" id="meetingTypeSwitch" name="meetingType" value="online">
+                <label class="form-check-label" for="meetingTypeSwitch" id="meetingTypeLabel">Face-to-Face</label>
+              </div>
+            </div>
+            <div class="mb-3">
+              <label for="eventUrl" class="form-label">Event URL (for Online Meetings)</label>
+              <input type="url" class="form-control" id="eventUrl" name="eventUrl" placeholder="http://example.com" disabled>
+            </div>
+            <div class="form-check mb-3">
+              <input class="form-check-input" type="checkbox" id="isRepeating" name="isRepeating">
+              <label class="form-check-label" for="isRepeating">Is this a repeating event?</label>
+            </div>
+            <button type="submit" class="btn btn-primary text-light">Add Appointment</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <script src="../js/user/profile_appointment.js"></script>
   <?php 
