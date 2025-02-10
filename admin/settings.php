97ed0328b5336329e780199fe9e33af027c825b3
Frankii <franklinoliveros2003@gmail.com>
2025-02-10 13:58:11 +0800

    admin setting nav

--- admin/settings.php
+++ admin/settings.php
@@ -1,22 +1,46 @@
+<?php
+session_start();
+
+if (isset($_SESSION['verification_status']) && $_SESSION['verification_status'] != 'Verified') {
+  header('location: ../user/verification.php');
+} else if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] != 0) {
+  header('location: ../index.php');
+}
+
+require_once('../tools/functions.php');
+require_once('../classes/account.class.php');
+
+?>
 <html lang="en">
-<?php 
-  $title = 'Admin | Settings';
-  include './includes/admin_head.php';
-  function getCurrentPage() {
-    return basename($_SERVER['PHP_SELF']);
-  }
+<?php
+$title = 'Admin | Settings';
+include './includes/admin_head.php';
+function getCurrentPage()
+{
+  return basename($_SERVER['PHP_SELF']);
+}
 ?>
 
 <link rel="stylesheet" href="./css/OnOffToggle.css">
+
 <body>
-  <?php 
-    require_once ('./includes/admin_header.php');
+  <?php
+  require_once('./includes/admin_header.php');
   ?>
-  <?php 
-    require_once ('./includes/admin_sidepanel.php');
+  <?php
+  require_once('./includes/admin_sidepanel.php');
   ?>
 
   <section id="dashboard" class="page-container">
+
+    <?php
+    $userManagement = 'active';
+    $aUserManagement = 'page';
+    $cUserManagement = 'text-dark';
+
+    include 'adminSettings_nav.php';
+    ?>
+
     <h1 class="text-start">User Group Management</h1>
 
     <div class="row flex-md-nowrap row-cols-1 row-cols-md-2 d-none d-md-flex">
@@ -240,4 +264,5 @@
   </section>
 
 </body>
+
 </html>
